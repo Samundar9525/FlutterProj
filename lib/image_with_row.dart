9cff17be1b88a4a87398fd4bc8133f71d41dd9ec
samundar <samundarsingh9525@gmail.com>
2021-11-12 21:27:08 +0530

    Contact type UI

--- lib/image_with_row.dart
+++ lib/image_with_row.dart
@@ -9,32 +9,99 @@ class myapp4 extends StatefulWidget {
 
 class _myapp4State extends State<myapp4> {
   @override
+  String text1="Samundar Singh";
+  String text2="Mukesh Patel";
+  String text3="Abhijeet Chakar.";
+  String text4="Sabbir Alam  ";
+  String text5="Ghanshyam";
+  String tem="  Calling";
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("image and row demo")),
       body: Center(
-        child: Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Hello Sam", textScaleFactor: 3.0,),
-            SizedBox(width: 40.0),
-            TextButton(
-              style: TextButton.styleFrom(
-                primary: Colors.white,
-                backgroundColor: Colors.teal,
-                onSurface: Colors.red,
-                textStyle: TextStyle(
-                    color: Colors.black,
-                    fontSize: 35,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSF1D-BYc9SEJFeEeftjtdD2ONIq0wjfW_P2g&usqp=CAU",
+                  fit:BoxFit.contain,
+                  height:50,
+                  width:50,
                 ),
-              ),
-              onPressed: () {
-                print('Pressed');
-              },
-              child: Text("Press me"),
-            )
+                SizedBox(width: 15.0),
+                Text(text1, textScaleFactor: 2.0,),
+
+                SizedBox(width: 65.0),
+                FlatButton(onPressed: (){
+                  text1="Calling ...";
+                  setState(() {
+                  });
+                }, child: Text("Call"),color: Colors.teal,textColor: Colors.white,)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSF1D-BYc9SEJFeEeftjtdD2ONIq0wjfW_P2g&usqp=CAU",
+                  fit:BoxFit.contain,
+                  height:50,
+                  width:50,
+                ),
+                SizedBox(width: 15.0),
+                Text(text2, textScaleFactor: 2.0,),
+                SizedBox(width: 65.0),
+                FlatButton(onPressed: (){
+                  text2="Calling ...";
+                  print(text1);
+                  setState(() {
+                  });
+                }, child: Text("Call"),color: Colors.teal,textColor: Colors.white,)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSF1D-BYc9SEJFeEeftjtdD2ONIq0wjfW_P2g&usqp=CAU",
+                  fit:BoxFit.contain,
+                  height:50,
+                  width:50,
+                ),
+                SizedBox(width: 15.0),
+                Text(text3, textScaleFactor: 2.0,),
+                SizedBox(width: 65.0),
+                FlatButton(onPressed: (){
+                  text3="Calling ...";
+                  print(text1);
+                  setState(() {
+                  });
+                }, child: Text("Call"),color: Colors.teal,textColor: Colors.white,)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSF1D-BYc9SEJFeEeftjtdD2ONIq0wjfW_P2g&usqp=CAU",
+                  fit:BoxFit.contain,
+                  height:50,
+                  width:50,
+                ),
+                SizedBox(width: 15.0),
+                Text(text4, textScaleFactor: 2.0,),
+                SizedBox(width: 65.0),
+                FlatButton(onPressed: (){
+                  text4="Calling ...";
+                  print(text1);
+                  setState(() {
+                  });
+                }, child: Text("Call"),color: Colors.teal,textColor: Colors.white,)
+              ],
+            ),
+
+
           ],
-        ),
+        )
       ),
     );
   }
