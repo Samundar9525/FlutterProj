096fe7eedc1033d2e3e3697c0e7f3da431d98713
samundar <samundarsingh9525@gmail.com>
2021-11-14 10:43:48 +0530

    Navigator and hero animation

--- lib/food_detail.dart
+++ lib/food_detail.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
 
 class fooddetail extends StatelessWidget {
-  const fooddetail({Key? key}) : super(key: key);
+  final foods;
+  const fooddetail({Key? key, @required this.foods}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Drink Detail')),
+      appBar: AppBar(
+        title: Text(foods['strMeal']),
+        elevation: 0.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Hero(
+                tag: foods["idMeal"],
+                child: CircleAvatar(
+                  radius: 150.0,
+                  backgroundImage: NetworkImage(foods['strMealThumb']),
+                )),
+            SizedBox(
+              height:30.0,
+            ),
+            Text("ID :${foods['idMeal']}",
+              style:TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w700,
+            )
+            ),
+            SizedBox(
+              height:10.0,
+            ),
+            Text("Item :${foods['strMeal']}",
+            style:TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w700,
+            )
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/listview_builder.dart
+++ lib/listview_builder.dart
@@ -12,9 +12,8 @@ class myapp5 extends StatefulWidget {
 }
 
 class _myapp5State extends State<myapp5> {
-  var api = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood";
-  var res,food;
-
+  var api = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian";
+  var res, food;
 
   @override
   void initState() {
@@ -22,40 +21,56 @@ class _myapp5State extends State<myapp5> {
     fetchData();
   }
 
-  fetchData()async
-  {
-    res=await http.get(Uri.parse(api));
-    food=jsonDecode(res.body)["meals"];
+  fetchData() async {
+    res = await http.get(Uri.parse(api));
+    food = jsonDecode(res.body)["meals"];
     print(food);
-    setState(() {
-
-    });
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:Text("ListView Builder")),
-      body:Center(
-        child: res!=null? ListView.builder(
-          itemCount: food.length,
-          itemBuilder: (context,index){
-            var foods=food[index];
-            return ListTile(
-              leading: CircleAvatar(backgroundImage: NetworkImage(foods['strMealThumb']),),
-              title: Text("${foods["strMeal"]}",
-                  style:TextStyle(fontSize: 15,color: Colors.blue)),
-              subtitle: Text("${foods["idMeal"]}",
-                  style:TextStyle(fontSize: 8,color: Colors.blue)),
-              onTap: (){
-                Navigator.push(context,MaterialPageRoute(builder: (context)=> fooddetail()));
-              },
+        appBar: AppBar(title: Text("ListView Builder")),
+        body: Center(
+          child: res != null
+              ? ListView.builder(
+                  itemCount: food.length,
+                  itemBuilder: (context, index) {
+                    var foods = food[index];
+                    return Container(
+                      height: 100,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          trailing: Icon(Icons.circle),
+                          leading: Hero(
+                              tag: foods["idMeal"],
+                              child: CircleAvatar(
+                                radius: 50,
 
-            );
-          },
-        ):CircularProgressIndicator(),
-      )
-
-    );
+                                backgroundImage:
+                                    NetworkImage(foods['strMealThumb']),
+                              )),
+                          title: Text("${foods["strMeal"]}",
+                              style: TextStyle(fontSize: 20, color: Colors.blue)),
+                          subtitle: Text("id: ${foods["idMeal"]}",
+                              style: TextStyle(fontSize: 14, color: Colors.black)),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => fooddetail(foods: foods),
+                                fullscreenDialog: true,
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                )
+              : CircularProgressIndicator(),
+        ));
   }
 }
